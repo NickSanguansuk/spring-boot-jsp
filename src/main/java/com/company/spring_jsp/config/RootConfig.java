0703365bf0d2c1eb8d7e72aa3b9e8bcb37f92d45
Wasin Sanguansuk <wasin_sanguansuk@hotmail.com>
2021-09-29 19:31:47 -0500

    Modified the AWS access key

--- src/main/java/com/company/spring_jsp/config/RootConfig.java
+++ src/main/java/com/company/spring_jsp/config/RootConfig.java
@@ -63,7 +63,7 @@ public class RootConfig {
     @Bean(name = "multipartResolver")
     public CommonsMultipartResolver multipartResolver() {
         CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-        multipartResolver.setMaxUploadSize(100000);
+        multipartResolver.setMaxUploadSize(1000000);
         multipartResolver.setPreserveFilename(true);
         return multipartResolver;
     }
