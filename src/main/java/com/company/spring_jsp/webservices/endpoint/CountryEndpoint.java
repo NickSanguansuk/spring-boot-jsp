168eb0e2db516dc91c54e73efa3e0d4d9e1fbcd4
Wasin Sanguansuk <wasin_sanguansuk@hotmail.com>
2021-10-04 15:47:11 -0500

    wsdl working

---
+++ src/main/java/com/company/spring_jsp/webservices/endpoint/CountryEndpoint.java
@@ -0,0 +1,31 @@
+package com.company.spring_jsp.webservices.endpoint;
+
+import com.company.spring_jsp.webservices.country.GetCountryRequest;
+import com.company.spring_jsp.webservices.country.GetCountryResponse;
+import com.company.spring_jsp.webservices.dao.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CountryEndpoint {
+    private static final String NAMESPACE_URI = "http://company.com/spring_jsp/webservices/country";
+
+    private CountryRepository countryRepository;
+
+    @Autowired
+    public CountryEndpoint(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        GetCountryResponse response = new GetCountryResponse();
+        response.setCountry(countryRepository.findCountry(request.getName()));
+
+        return response;
+    }
+}
